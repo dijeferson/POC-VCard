cbf4f6e0428950825bda2c1d5f8e1ef58dd1ab60
Jeferson Santos <jefersons@ciandt.com>
2015-10-15 16:57:52 -0300

    .

---
+++ POC-VCard/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace POC_VCard
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+
+        private async void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            string template = @"BEGIN:VCARD
+FN:{0}
+TEL;Home:{1}
+REV:20090401T065518
+VERSION:2.1
+END:VCARD";
+
+            var vcardData = string.Format(template, txtNome.Text, txtTelefone.Text);
+
+            StorageFolder folder = ApplicationData.Current.LocalFolder;
+            StorageFile vcard = await folder.CreateFileAsync("test.vcf", CreationCollisionOption.ReplaceExisting);
+            await FileIO.WriteTextAsync(vcard, vcardData);
+
+            await Launcher.LaunchFileAsync(vcard);
+        }
+    }
+}
